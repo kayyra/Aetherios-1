1cea983e3f33fd3dce375a78f9d861168acc0c39
kayyra <anarodriguez.p@hotmail.es>
2023-06-13 08:07:24 +0200

    fixed menu

--- js/menu-hamburguesa.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("hamburguer-menu").addEventListener("click", function () {
-    let menu = document.getElementById('hamburguer-menu');
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-});
